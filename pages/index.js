1bd0e49add856eee2c7cb3f9fd1161c5fc07c210
Jacob Friedmann <jfriedmann@rover.com>
2020-06-17 12:12:55 -0700

    Add Bubble Heatmap

--- pages/index.js
+++ pages/index.js
@@ -5,42 +5,76 @@ import { Col } from "react-bootstrap";
 import { Row } from "react-bootstrap";
 import BrutalityByState from "components/widgets/BrutalityByState";
 import BrutalityOverTime from "components/widgets/BrutalityOverTime";
+import BubbleHeatmap from "components/widgets/BubbleHeatmap";
 
 function HomePage() {
   return (
     <div>
+      <Container
+        style={{ width: "1200px", backgroundColor: "#000", padding: "1em" }}
+      >
+        <Row>
+          <Col sm={1}>
+            <img
+              alt="Grassroots Law Project"
+              width={261}
+              height={60}
+              src="https://images.squarespace-cdn.com/content/5e7662fe45630059ae9347cf/1591116153336-2V6MDHUQAXMOIKOZI6DJ/glp+text+only+logo+neon.png?format=1500w&content-type=image%2Fpng"
+            />
+            ​
+          </Col>
+          <Col sm={9}>
+            {" "}
+            <h2
+              align="center"
+              style={{
+                font: "helvetica-neue",
+                fontWeight: "heavy",
+                color: "#66fcf1",
+              }}
+            >
+              Police Brutality Dashboard
+            </h2>
+          </Col>
+          <Col sm={2}>
+            {" "}
+            <Button
+              style={{ backgroundColor: "#66fcf1", color: "#000" }}
+              variant="link"
+              href="https://secure.actblue.com/donate/glp-homepage?refcode=homepage_nav"
+            >
+              DONATE
+            </Button>
+          </Col>
+        </Row>
+      </Container>
 
-<Container style={{width:"1200px",backgroundColor: "#000",padding: "1em"}}>
-  <Row>
-    <Col sm={1}>
-    <img
-                width={261}
-                height={60}            
-    src="https://images.squarespace-cdn.com/content/5e7662fe45630059ae9347cf/1591116153336-2V6MDHUQAXMOIKOZI6DJ/glp+text+only+logo+neon.png?format=1500w&content-type=image%2Fpng"
-  />
-​
-    </Col>
-    <Col sm={9}> <h2 align="center" style={{font:"helvetica-neue",fontWeight:"heavy",color: "#66fcf1"}}>Police Brutality Dashboard</h2>
-</Col>
-<Col sm={2}>  <Button style={{backgroundColor:"#66fcf1",color:"#000"}} variant="link" a href="https://secure.actblue.com/donate/glp-homepage?refcode=homepage_nav">DONATE</Button>
-</Col>
-  </Row>
-  
-</Container>
-
-<Container>
-  <Row>
-    <Col><BrutalityOverTime /></Col>
-    <Col md = "auto"><BrutalityByState /></Col>
-  </Row>
-  <Row>
-    <Col><BrutalityOverTime /></Col>
-    <Col><BrutalityOverTime /></Col>
-    <Col><BrutalityOverTime /></Col>
-  </Row>
-</Container>
-
-
+      <Container>
+        <Row>
+          <Col>
+            <BrutalityOverTime />
+          </Col>
+          <Col md="auto">
+            <BrutalityByState />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <BrutalityOverTime />
+          </Col>
+          <Col>
+            <BrutalityOverTime />
+          </Col>
+          <Col>
+            <BrutalityOverTime />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <BubbleHeatmap />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
