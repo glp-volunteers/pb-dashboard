fac72cbe2a09d0675056921d9691524cc1ba72fa
Tyler Broussard <tylerjbroussard@live.com>
2020-06-22 17:30:07 -0700

    Update BrutalityByState.js to pull live data via policetracker
    Added fetch call to script to pull live data to Victory Bar widget.

--- components/widgets/BrutalityByState.js
+++ components/widgets/BrutalityByState.js
@@ -1,95 +1,68 @@
 import React from "react";
-import { VictoryChart, VictoryBar, VictoryTheme, VictoryZoomContainer } from "victory";
+import {
+  VictoryChart,
+  VictoryBar,
+  VictoryTheme,
+  VictoryZoomContainer,
+} from "victory";
 
 import { COLORS } from "styles/constants";
 
-const mockData = [
-  { name: "CA", value: 1186 },
-  { name: "TX", value: 719 },
-  { name: "FL", value: 540 },
-  { name: "AZ", value: 343 },
-  { name: "GA", value: 265 },
-  { name: "CO", value: 227 },
-  { name: "WA", value: 218 },
-  { name: "OH", value: 215 },
-  { name: "OK", value: 214 },
-  { name: "NC", value: 204 },
-  { name: "MO", value: 197 },
-  { name: "IL", value: 181 },
-  { name: "TN", value: 179 },
-  { name: "PA", value: 171 },
-  { name: "NY", value: 155 },
-  { name: "LA", value: 148 },
-  { name: "NM", value: 142 },
-  { name: "AL", value: 138 },
-  { name: "MD", value: 128 },
-  { name: "VA", value: 124 },
-  { name: "IN", value: 124 },
-  { name: "MI", value: 123 },
-  { name: "KY", value: 122 },
-  { name: "NV", value: 121 },
-  { name: "SC", value: 120 },
-  { name: "OR", value: 113 },
-  { name: "WI", value: 111 },
-  { name: "MS", value: 108 },
-  { name: "AR", value: 103 },
-  { name: "NJ", value: 103 },
-  { name: "MN", value: 84 },
-  { name: "UT", value: 82 },
-  { name: "KS", value: 75 },
-  { name: "WV", value: 74 },
-  { name: "MA", value: 56 },
-  { name: "ID", value: 48 },
-  { name: "IA", value: 45 },
-  { name: "AK", value: 41 },
-  { name: "MT", value: 40 },
-  { name: "HI", value: 37 },
-  { name: "NE", value: 36 },
-  { name: "CT", value: 36 },
-  { name: "ME", value: 31 },
-  { name: "DC", value: 26 },
-  { name: "SD", value: 25 },
-  { name: "DE", value: 20 },
-  { name: "WY", value: 19 },
-  { name: "NH", value: 17 },
-  { name: "VT", value: 12 },
-  { name: "ND", value: 11 },
-  { name: "RI", value: 6 },
-];
+class BrutalityByState extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      output: [],
+      isLoaded: false,
+    };
+  }
 
-function transformData(data) {
-  return data
-    .map(function (d) {
-      return { x: d.name, y: d.value };
-    })
-    .sort(function (a, b) {
-      return a.y - b.y;
-    });
-}
+  componentDidMount() {
+    fetch(
+      "http://cors-anywhere.herokuapp.com/policetracker.link/count/shootings/state/abbv"
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          output: json.sort(function (a, b) {
+            return a.total - b.total;
+          }),
+        });
+      });
+  }
 
-function BrutalityByState() {
-  return (
-    <div style={{ width: 500 }}>
-      <h2>Police Killings By State</h2>
-      <VictoryChart
-        height={800}
-        theme={VictoryTheme.material}
-        domainPadding={{ x: 0 }}
-        containerComponent={<VictoryZoomContainer zoomDomain={{x: [1, 51]}}/>}
-
-      >
-
-        <VictoryBar
-          barRatio={0.8}
-          style={{
-            data: { fill: COLORS.primary },
-          }}
-          horizontal
-          data={transformData(mockData)}
-        />
-      </VictoryChart>
-    </div>
-  );
+  render() {
+    var { isLoaded, output } = this.state;
+    if (!isLoaded) {
+      return <div> Loading...</div>;
+    } else {
+      return (
+        <div style={{ width: 500 }}>
+          <h2>Police Killings By State</h2>
+          <VictoryChart
+            height={800}
+            theme={VictoryTheme.material}
+            domainPadding={{ x: 0 }}
+            containerComponent={
+              <VictoryZoomContainer zoomDomain={{ x: [1, 51] }} />
+            }
+          >
+            <VictoryBar
+              barRatio={0.8}
+              style={{
+                data: { fill: COLORS.primary },
+              }}
+              horizontal
+              data={output}
+              x="state_code"
+              y="total"
+            />
+          </VictoryChart>
+        </div>
+      );
+    }
+  }
 }
 
 export default BrutalityByState;
