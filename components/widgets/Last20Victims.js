7a00f5d026db5c620a00afc1e4d5cbb994148f91
Alex Marcus <amarcus@adobe.com>
2020-06-26 21:51:45 -0700

    Prints dates in human readable format

--- components/widgets/Last20Victims.js
+++ components/widgets/Last20Victims.js
@@ -1,62 +1,54 @@
-import React, {Component} from 'react';
-
-
+import React, { Component } from "react";
 
 class Last20Victims extends React.Component {
- 
-    constructor (props) {
-        super(props);
-        this.state= {
-            items:[],
-            isLoaded: false, 
-        }
-    }
-
-    componentDidMount(){
-
-        fetch('http://cors-anywhere.herokuapp.com/policetracker.link/shootings/last20')
-        .then(res => res.json())
-        .then(json=> {   
-            this.setState({
-                isLoaded: true,
-                items: json,
-            })
-
-        });
-    }
- 
-    render(){
-
-        var{isLoaded,items}= this.state;
-        if (!isLoaded) {
-            return <div> Loading...</div>
-        }
-
-        else {
-
-            return(
-                <div className= "Last20Victims">
-                <ul >
-
-                    {items.map(item => (
-                        <li key={item.id}>
-                            
-
-                        <b> <a target="_blank" href= {item.media_link}> {item.victim_name}</a>
-                            </b> killed in {item.state} State. Reported on {item.date}
-                        </li>
-                    ))};
-                    
-                </ul>
-
-                </div>
-    
-
-            )
-        }
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
     };
-}
+  }
 
+  componentDidMount() {
+    fetch(
+      "http://cors-anywhere.herokuapp.com/policetracker.link/shootings/last20"
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        });
+      });
+  }
+
+  render() {
+    var { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return <div> Loading...</div>;
+    } else {
+      return (
+        <div className="Last20Victims">
+          <ul>
+            {items.map((item) => {
+              const humanDate = new Date(item.date).toDateString();
+              return (
+                <li key={item.id}>
+                  <b>
+                    <a target="_blank" rel="noreferrer" href={item.media_link}>
+                      {item.victim_name}
+                    </a>
+                  </b>
+                  &nbsp;was killed in {item.state} State. Reported on&nbsp;
+                  {humanDate}.
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      );
+    }
+  }
+}
 
 export default Last20Victims;
