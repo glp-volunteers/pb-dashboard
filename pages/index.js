f4cb9f50757d08e8863bb359f79eb47714454a54
tyler j broussard <tylerjbroussard@live.com>
2020-06-27 20:55:14 -0700

    Connected BrutalityOverTime widget to mysql

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,12 @@ export const getServerSideProps = async () => {
   const shootingsByState = await getApiData("count/shootings/state/name");
   const populationDataRaw = await fetch(
     "https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest"
-  );
+    );
+  const shootingsOverTimeRaw = await getApiData("count/shootings/overtime");
   const populationData = await populationDataRaw.json();
+
+  const shootingsOverTime = await JSON.parse(JSON.stringify(shootingsOverTimeRaw));
+
   const filteredShootingsByState = shootingsByState.filter((state) =>
     populationData.data.find((s) => s.State === state.state)
   );
@@ -29,11 +33,12 @@ export const getServerSideProps = async () => {
     props: {
       shootingsByState: filteredShootingsByState,
       shootingsPerCapita,
+      shootingsOverTime,
     },
   };
 };
 
-function HomePage({ shootingsByState, shootingsPerCapita }) {
+function HomePage({ shootingsByState, shootingsPerCapita, shootingsOverTime }) {
   const [isPerCapita, setIsPerCapita] = useState(false);
 
   return (
@@ -62,7 +67,7 @@ function HomePage({ shootingsByState, shootingsPerCapita }) {
         <Col lg={4}>
           <h2>Most Recent Police Killings</h2>
           <Last20Victims />
-          <BrutalityOverTime />
+          <BrutalityOverTime data={shootingsOverTime}/>
         </Col>
       </Row>
       <Row className="mt-3">
