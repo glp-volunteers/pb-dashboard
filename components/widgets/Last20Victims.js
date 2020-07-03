452b652351a84e2dcb4299fc40bfe366ce63a79c
Jacob Friedmann <jfriedmann@rover.com>
2020-07-03 10:28:32 -0700

    Add real data and data migration script

--- components/widgets/Last20Victims.js
+++ components/widgets/Last20Victims.js
@@ -1,8 +1,6 @@
 import React from "react";
 import SimpleBarReact from "simplebar-react";
 
-
-
 class Last20Victims extends React.Component {
   constructor(props) {
     super(props);
@@ -29,26 +27,28 @@ class Last20Victims extends React.Component {
       return <div> Loading...</div>;
     } else {
       return (
-
-        <div className="Last20Victims"  >
-          <SimpleBarReact style={{ maxHeight: 450}}>
-          <ul >
-            {items.map((item) => {
-              const humanDate = new Date(item.date).toDateString();
-              return (
-
-                <li key={item.shootingsID} > 
-                  <b>
-                    <a target="_blank" rel="noreferrer" href={item.media_link}>
-                      {item.victim_name}
-                    </a>
-                  </b>
-                  &nbsp;was killed in {item.state} State. Reported on&nbsp;
-                  {humanDate}.
-                </li>
-              );
-            })}
-          </ul>
+        <div className="Last20Victims">
+          <SimpleBarReact style={{ maxHeight: 450 }}>
+            <ul>
+              {items.map((item) => {
+                const humanDate = new Date(item.date).toDateString();
+                return (
+                  <li key={item.shootingsID}>
+                    <b>
+                      <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href={item.media_link}
+                      >
+                        {item.victim_name}
+                      </a>
+                    </b>
+                    &nbsp;was killed in {item.state} State. Reported on&nbsp;
+                    {humanDate}.
+                  </li>
+                );
+              })}
+            </ul>
           </SimpleBarReact>
         </div>
       );
